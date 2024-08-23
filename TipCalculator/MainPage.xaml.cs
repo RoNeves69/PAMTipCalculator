aae94f11c7b0e135750476ba82755935d95aebec
RoNeves69 <RonaldNeves69@outlook.com>
2024-08-23 20:51:34 -0300

    Fiz a Interface

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetHighTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
